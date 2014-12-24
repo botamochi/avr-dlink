bc48a652f3a043f4463c222b00dfbfe8eed7cf23
botamochi <botamochi0914@yahoo.co.jp>
2014-12-24 21:26:33 +0900

    [Add]サンプルプログラムsandbag.cを追加

---
+++ samples/sandbag.c
@@ -0,0 +1,62 @@
+//--------------------------------------------------
+//
+// 育成ギア用サンドバッグ
+// バトルは初代ギアの形式で、必ず勝てる
+// 通信の開始は必ずマイコン側から
+//
+// 2014/12/24 作成
+//--------------------------------------------------
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "serial.h"
+#include "dgear.h"
+
+//==================================================
+// 送信するフレーム
+// バージョン1のアグモン
+// 負けで固定
+//==================================================
+#define FRAME1 0b1110110000010011
+#define FRAME2 0b1111110100000010
+
+//==================================================
+// main
+//==================================================
+int main(void)
+{
+  dframe d1, d2;
+
+  serial_init(9600);
+  dgear_initialize();
+  sei();
+  while (1) {
+    /* ターミナルからのデータ受信で通信開始 */
+    serial_getc();
+
+    /* 最初のフレームを送信 */
+    dgear_connect();
+    dgear_put_frame(FRAME1);
+
+    /* 受信 */
+    while (!dgear_available());
+    d1 = dgear_get_frame();
+    _delay_ms(2);
+
+    /* 2つめのフレームを送信 */
+    dgear_connect();
+    dgear_put_frame(FRAME2);
+
+    /* 受信 */
+    while (!dgear_available());
+    d2 = dgear_get_frame();
+
+    /* 結果をターミナルに送信 */
+    serial_puts("---- receive data ----\r\n");
+    serial_puts("1:");
+    serial_println(d1, BIN);
+    serial_puts("2:");
+    serial_println(d2, BIN);
+  }
+  return 0;
+}
